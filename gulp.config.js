4617a1f9a870857ba71f1d1fd6177761313ad9d0
rmabini <mabini.roberto@gmail.com>
2017-03-23 21:12:21 +0800

    Added custom css

--- gulp.config.js
+++ gulp.config.js
@@ -27,6 +27,7 @@ module.exports = function() {
     build: './build/',
     client: client,
     css: temp + 'styles.css',
+    customCss: temp + 'pexStyle.css',
     fonts: bower.directory + 'font-awesome/fonts/**/*.*',
     html: client + '**/*.html',
     htmltemplates: clientApp + '**/*.html',
